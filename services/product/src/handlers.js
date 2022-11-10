1b295e99fbe76da35346a3a3b493a19a60380453
Mikhail Unenov <mikhail_unenov@epam.com>
2022-11-10 14:31:41 +0400

    feature(@lib/config): Able to choose config section to be returned by factory (#12)
    * test(@lib/create-config): add a passing config section use case
    
    * feat(@lib/create-config): allow to choose config section to return
    
    * feat(@services/product): load config with db section only

--- services/product/src/handlers.js
+++ services/product/src/handlers.js
@@ -1,14 +1,15 @@
 import { createEventHandlers } from '@lib/create-event-handlers';
 import { createDb } from '@lib/create-db';
-import { createConfig } from '@lib/create-config';
+import { ConfigSections, createConfig } from '@lib/create-config';
 
 import { handler as createCreateProduct } from './create-create-product';
 import { handler as createGetProductById } from './create-get-product-by-id';
 import { handler as createGetProductList } from './create-get-product-list';
 import { createRepo } from './create-repo';
 
-const config = createConfig();
-const db = createDb(config.db);
+const config = createConfig(ConfigSections.DB);
+const db = createDb(config[ConfigSections.DB]);
+const repo = createRepo({ db });
 
 export const { createProduct, getProductById, getProductList } =
   createEventHandlers({
@@ -17,5 +18,5 @@ export const { createProduct, getProductById, getProductList } =
       getProductById: createGetProductById,
       getProductList: createGetProductList,
     },
-    context: { repo: createRepo({ db }) },
+    context: { repo },
   });
