077db0a2104cc47d5534960bed451d4e898f61d8
Mikhail Unenov <mikhail_unenov@epam.com>
2022-11-13 12:34:00 +0400

    feat(@services/product): inject notifier

--- services/product/src/handlers.js
+++ services/product/src/handlers.js
@@ -1,5 +1,6 @@
 import { createEventHandlers } from '@lib/create-event-handlers';
 import { createDb } from '@lib/create-db';
+import { createNotifier } from '@lib/create-notifier';
 import { ConfigSections, createConfig } from '@lib/create-config';
 
 import { handler as createCatalogBatchProcess } from './create-catalog-batch-process';
@@ -8,9 +9,11 @@ import { handler as createGetProductById } from './create-get-product-by-id';
 import { handler as createGetProductList } from './create-get-product-list';
 import { createRepo } from './create-repo';
 
-const config = createConfig(ConfigSections.DB);
+const config = createConfig(ConfigSections.DB, ConfigSections.NOTIFIER);
 const db = createDb(config[ConfigSections.DB]);
 const repo = createRepo({ db });
+const notifier = createNotifier(config[ConfigSections.NOTIFIER]);
+const handlerConfig = { topics: config[ConfigSections.NOTIFIER].topics };
 
 export const {
   catalogBatchProcess,
@@ -24,5 +27,5 @@ export const {
     getProductById: createGetProductById,
     getProductList: createGetProductList,
   },
-  context: { repo },
+  context: { config: handlerConfig, repo, notifier },
 });
